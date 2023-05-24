8e2a457a40e06e7a4b1d0104f7c43c3f288ff44b
Peter Jones <pjxcvi@proton.me>
2023-05-24 18:32:00 -0400

    refactor

--- src/caesar.js
+++ src/caesar.js
@@ -3,16 +3,18 @@ const caesarModule = (function () {
 
   function caesar(input, shift, encode = true) {
     if (!shift || shift > 25 || shift < -25 || shift === 0) {
-      return false; 
+      return false;
     }
 
     const lowerCasedInput = input.toLowerCase();
-    let result = '';
+    let result = "";
 
     for (const char of lowerCasedInput) {
       if (alphabet.includes(char)) {
         const index = alphabet.indexOf(char);
-        let newIndex = encode ? (index + shift + 26) % 26 : (index - shift + 26) % 26;
+        let newIndex = encode
+          ? (index + shift + 26) % 26
+          : (index - shift + 26) % 26;
         const encryptedChar = alphabet[newIndex];
         result += encryptedChar;
       } else {
