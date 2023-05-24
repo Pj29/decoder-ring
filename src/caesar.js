32ed428efbc2f57e3fa2e3c0473bcf1f5aadf704
Peter Jones <pjxcvi@proton.me>
2023-05-24 18:26:32 -0400

    refactor

--- src/caesar.js
+++ src/caesar.js
@@ -1,23 +1,26 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
-  // define the english alphabet and split it for spaces 
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
   function caesar(input, shift, encode = true) {
-    // your solution code here
     if (!shift || shift > 25 || shift < -25 || shift === 0) {
-      return false;
+      return false; 
     }
+
     const lowerCasedInput = input.toLowerCase();
-    const result = lowerCasedInput.map(function (char) {
-      if (!/[a-z]/.test(char)) {
-        return char;
+    let result = '';
+
+    for (const char of lowerCasedInput) {
+      if (alphabet.includes(char)) {
+        const index = alphabet.indexOf(char);
+        let newIndex = encode ? (index + shift +26) % 26 : (index - shift + 26) % 26;
+        const encryptedChar = alphabet[newIndex];
+        result += encryptedChar;
+      } else {
+        result += char;
       }
-    });
+    }
+
+    return result;
   }
 
   return {
